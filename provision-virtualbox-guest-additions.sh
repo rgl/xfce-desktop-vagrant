a0d30c088b5e43b3b3b7126aeb997559a5be1b12
Rui Lopes <rgl@ruilopes.com>
2017-07-23 15:33:43 +0100

    workaround a bug in the virtualbox guest additions installation

--- provision-virtualbox-guest-additions.sh
+++ provision-virtualbox-guest-additions.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 set -eux
+# install the VirtualBox Guest Additions.
+# this will be installed at /opt/VBoxGuestAdditions-VERSION.
+# NB You can unpack the VBoxLinuxAdditions.run file contents with:
+#       VBoxLinuxAdditions.run --target /tmp/VBoxLinuxAdditions.run.contents --noexec
+# NB REMOVE_INSTALLATION_DIR=0 is to fix a bug in VBoxLinuxAdditions.run.
+#    See http://stackoverflow.com/a/25943638.
+apt-get -y -q install gcc dkms
+mkdir -p /mnt
 mount /dev/sr0 /mnt
-/mnt/VBoxLinuxAdditions.run
+while [ ! -f /mnt/VBoxLinuxAdditions.run ]; do sleep 1; done
+# NB we ignore exit code 2 (cannot find vboxguest module) because of what
+#    seems to be a bug in VirtualBox 5.1.20. there isn't actually a problem
+#    loading the module.
+REMOVE_INSTALLATION_DIR=0 /mnt/VBoxLinuxAdditions.run --target /tmp/VBoxGuestAdditions || [ $? -eq 2 ]
+rm -rf /tmp/VBoxGuestAdditions
+umount /mnt
 eject /dev/sr0
+modinfo vboxguest
