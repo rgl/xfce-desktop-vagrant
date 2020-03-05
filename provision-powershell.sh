e0bc7baf09f7e58f66ac14c5f51896ece491d872
Rui Lopes <rgl@ruilopes.com>
2020-03-05 21:53:48 +0000

    upgrade to powershell 7.0.0

--- provision-powershell.sh
+++ provision-powershell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-powershell_version=7.0.0-rc.3
-powershell_url="https://github.com/PowerShell/PowerShell/releases/download/v${powershell_version}/powershell-lts-${powershell_version}-linux-x64.tar.gz"
+powershell_version=7.0.0
+powershell_url="https://github.com/PowerShell/PowerShell/releases/download/v${powershell_version}/powershell-${powershell_version}-linux-x64.tar.gz"
 powershell_filename="$(basename $powershell_url)"
 wget -q $powershell_url
 install -d /opt/powershell
