484b9157b4105f99de815e3c3778dad05d045872
Rui Lopes <rgl@ruilopes.com>
2020-04-26 09:36:19 +0100

    disable powershell telemetry

--- provision-powershell.sh
+++ provision-powershell.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
+# disable telemetry.
+echo 'export POWERSHELL_TELEMETRY_OPTOUT=1' >/etc/profile.d/disable-powershell-telemetry.sh
+echo 'export DOTNET_CLI_TELEMETRY_OPTOUT=1' >/etc/profile.d/disable-dotnet-cli-telemetry.sh
+source /etc/profile.d/disable-powershell-telemetry.sh
+source /etc/profile.d/disable-dotnet-cli-telemetry.sh
+
+# install.
 powershell_version=7.0.0
 powershell_url="https://github.com/PowerShell/PowerShell/releases/download/v${powershell_version}/powershell-${powershell_version}-linux-x64.tar.gz"
 powershell_filename="$(basename $powershell_url)"
